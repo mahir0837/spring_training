17bca0ecef7d81162d66b7ae60292735f1927026
owner <mahirsarac89@gmail.com>
2023-02-20 17:04:13 -0500

    Container created

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/sarac/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/sarac/bean_annotation/ComputerTest.java
@@ -1,9 +1,13 @@
 package com.sarac.bean_annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ComputerTest {
 
     public static void main(String[] args) {
 
+        ApplicationContext container=new AnnotationConfigApplicationContext();
 
     }
 }
