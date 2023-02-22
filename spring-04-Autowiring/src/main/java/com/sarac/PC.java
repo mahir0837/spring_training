331f4d94bca680b176471311cffe193c4801d035
owner <mahirsarac89@gmail.com>
2023-02-22 10:36:28 -0500

    AutoWiring

---
+++ spring-04-Autowiring/src/main/java/com/sarac/PC.java
@@ -0,0 +1,35 @@
+package com.sarac;
+
+import com.sarac.casefactory.Case;
+import com.sarac.monitorfactory.Monitor;
+import com.sarac.motherboardfactory.Motherboard;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@AllArgsConstructor
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+//    @Autowired Spring injection instead of us!!
+//    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+//        this.theCase = theCase;
+//        this.monitor = monitor;
+//        this.motherboard = motherboard;
+//    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt();
+    }
+}
