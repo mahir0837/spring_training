924ab9b79cc13e5ba7868b00ce92bb0594931ee0
owner <mahirsarac89@gmail.com>
2023-02-24 13:16:12 -0500

    SpringBoot training

--- spring-07-SpringBoot-Demo/src/main/java/com/sarac/saracApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/sarac/saracApplication.java
@@ -1,5 +1,7 @@
 package com.sarac;
 
+import com.sarac.model.Comment;
+import com.sarac.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +10,14 @@ import org.springframework.context.ApplicationContext;
 public class saracApplication {
 
     public static void main(String[] args) {
-       SpringApplication.run(saracApplication.class, args);
+
+        Comment comment=new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+      ApplicationContext container= SpringApplication.run(saracApplication.class, args);
+        CommentService commentService=container.getBean(CommentService.class);
+        commentService.publishComment(comment);
     }
 
 }
