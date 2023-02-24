04ff779c6b73d7f2ddfbfc7c92539f7b22a04dcb
owner <mahirsarac89@gmail.com>
2023-02-24 10:52:34 -0500

    All Package are created

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/sarac/saracApplication.java
@@ -0,0 +1,14 @@
+package com.sarac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class saracApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(saracApplication.class, args);
+    }
+
+}
---
+++ spring-07-SpringBoot-Demo/src/test/java/com/sarac/saracApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sarac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class saracApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
