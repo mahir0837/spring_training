e5b96b2b535a3ef5bafefb8d1e0ce18b56b2927d
owner <mahirsarac89@gmail.com>
2023-02-27 13:06:00 -0500

    MVC Model practice

--- spring-09-MVC-model/src/main/java/com/sarac/controller/StudentController.java
+++ spring-09-MVC-model/src/main/java/com/sarac/controller/StudentController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -13,6 +18,18 @@ public class StudentController {
         //Method Model Interface
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+        String subject="SpringBoot";
+        model.addAttribute("subject",subject);
+        int studentID=new Random().nextInt();
+        model.addAttribute("studentID",studentID);
+        List<Integer>number=new ArrayList<>();
+        number.add(4);
+        number.add(5);
+        number.add(6);
+        number.add(7);
+        model.addAttribute("numbers",number);
+        LocalDate ld=LocalDate.now();
+        model.addAttribute(ld);
         return "/student/welcome";
     }
 }
