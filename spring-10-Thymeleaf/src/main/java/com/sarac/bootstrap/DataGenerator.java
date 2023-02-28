d7038c48a6ef512b5d603b4d03dcc34c67b57442
owner <mahirsarac89@gmail.com>
2023-02-28 13:48:16 -0500

    ThymeleafPractice

---
+++ spring-10-Thymeleaf/src/main/java/com/sarac/bootstrap/DataGenerator.java
@@ -0,0 +1,33 @@
+package com.sarac.bootstrap;
+
+import com.github.javafaker.Faker;
+import com.sarac.model.Student;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student>createStudent(){
+
+        List<Student>students= Arrays.asList(
+                new Student(new Faker().name().firstName(),
+                        new Faker().name().lastName(),
+                        new Faker().number().numberBetween(20,50),
+                        new Faker().address().state()),
+        new Student(new Faker().name().firstName(),
+                new Faker().name().lastName(),
+                new Faker().number().numberBetween(20,50),
+                new Faker().address().state()),
+        new Student(new Faker().name().firstName(),
+                new Faker().name().lastName(),
+                new Faker().number().numberBetween(20,50),
+                new Faker().address().state()),
+        new Student(new Faker().name().firstName(),
+                new Faker().name().lastName(),
+                new Faker().number().numberBetween(20,50),
+                new Faker().address().state()));
+
+        return students;
+    }
+}
---
+++ spring-10-Thymeleaf/src/main/java/com/sarac/controller/StudentController.java
@@ -0,0 +1,23 @@
+package com.sarac.controller;
+
+import com.sarac.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "/student/register";
+    }
+    @RequestMapping("/drop")
+    public String drop(){
+        return "student/register";
+    }
+}
