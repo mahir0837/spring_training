d534147a92a3928f9db98c240f99c0633d5d01f6
owner <mahirsarac89@gmail.com>
2023-04-04 12:08:19 -0400

    Entity and Enum added

--- spring-11-Data-TableCreation/src/main/java/com/sarac/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/sarac/entity/Student.java
@@ -1,12 +1,32 @@
 package com.sarac.entity;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.sarac.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime birthDateTime;
+    @Enumerated(EnumType.STRING)
+//    @Enumerated(EnumType.ORDINAL) for integer
+    private Gender gender;
+    @Transient
+    private String city;
+
 }
