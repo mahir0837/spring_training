08857e41e780475943d5c577da734ad33d81ea60
owner <mahirsarac89@gmail.com>
2023-05-24 13:50:35 -0400

    Consuming_Api

---
+++ spring-17-REST-ConsumingApis/src/test/java/com/sarac/spring17/Spring17RestConsumingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarac.spring17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17RestConsumingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
