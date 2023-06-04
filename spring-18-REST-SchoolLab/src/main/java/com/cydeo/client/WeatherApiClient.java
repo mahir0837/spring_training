a64e96429866d4ac215a96c272671b5445d14ce1
owner <mahirsarac89@gmail.com>
2023-06-04 12:00:43 -0400

    Feign client dependency added

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/client/WeatherApiClient.java
@@ -0,0 +1,14 @@
+package com.cydeo.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "http://api.weatherstack.com",name = "WEATHER-CLIENT")
+public interface WeatherApiClient {
+
+    //http://api.weatherstack.com/current?access_key=d0b882f54e31fee3bbbea35dc4dfe761&query=London
+    @GetMapping("/current")
+    Object getCurrentWeather(@RequestParam(value = "access_key")String accessKey,
+                             @RequestParam(value="query") String city);
+}
